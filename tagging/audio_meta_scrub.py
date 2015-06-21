1fca1e7a79cce013b776eeae6088230425005f9a
Corey Goldberg <cgoldberg@gmail.com>
2015-06-20 20:56:33 -0400

    refactor

--- tagging/audio_meta_scrub.py
+++ tagging/audio_meta_scrub.py
@@ -4,12 +4,13 @@
 
 
 """
-Walk directories, clean, and tag audio files.
-Supports MP3 and FLAC formats only.
+Walk directories, clean and tag audio files.
+Supports MP3 and FLAC formats.
 
-Clear pictures.
-Clear all tags and metadata.
-Re-tag with 'Artist - Title' from filename.
+Filenames must be in the format: 'Artist - Title.mp3' or 'Artist - Title.flac'.
+(spaces, dashes, and multiple words are fine; but must contain " - " to delimit
+Artist and Title.  Existing metadata is deleted and new tags (artist/title only)
+are derived from the filename.
 """
 
 
@@ -22,30 +23,34 @@ start_dir = './test_files/.'
 
 
 
+def retag(filepath):
+    print('Loading File: %r' % filepath)
+    audio = File(filepath, easy=True)
+    if audio is None:
+        print('Invalid Audio File: %r' % filepath)
+    else:
+        artist, title = os.path.splitext(filename)[0].split(' - ', 1)
+        if 'audio/x-mp3' in audio.mime:
+            audio.delete()
+            audio['artist'] = artist
+            audio['title'] = title
+            audio.save(v1=0, v2_version=3)
+        elif 'audio/x-flac' in audio.mime:
+            audio.delete()
+            audio.clear_pictures()
+            audio['artist'] = artist
+            audio['title'] = title
+            audio.save(deleteid3=True)
+        else:
+            print('Invalid Audio File: %r' % filepath)
+        print(audio.pprint())
+    print('')
+
+
 if __name__ == '__main__':
     for root, dirs, files in os.walk(start_dir):
         for filename in files:
             filepath = os.path.abspath(os.path.join(root, filename))
-            print('Loading File: %r' % filepath)
-            audio = File(filepath, easy=True)
-            if audio is None:
-                print('Invalid Audio File: %r' % filepath)
-            else:
-                artist, title = os.path.splitext(filename)[0].split(' - ', 1)
-                if 'audio/x-mp3' in audio.mime:
-                    audio.delete()
-                    audio['artist'] = artist
-                    audio['title'] = title
-                    audio.save(v1=0, v2_version=3)
-                elif 'audio/x-flac' in audio.mime:
-                    audio.delete()
-                    audio.clear_pictures()
-                    audio['artist'] = artist
-                    audio['title'] = title
-                    audio.save(deleteid3=True)
-                else:
-                    print('Invalid Audio File: %r' % filepath)
-                print(audio.pprint())
-            print('')
+            retag(filepath)
     print('Done.\nProcessed %d files.' % len(files))
 
