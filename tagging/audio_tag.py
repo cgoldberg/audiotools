5e46ce3ae6cab7d0a9eb04c11a0070cb88830fce
Corey Goldberg <cgoldberg@gmail.com>
2018-01-15 09:38:37 -0500

    updates

--- tagging/audio_tag.py
+++ tagging/audio_tag.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
-# Corey Goldberg, 2017
+# Corey Goldberg, 2017, 2018
 # MIT License
 
 """
-Clean and tag audio files.
+Clean and tag a directory of audio files.
+
 Supports MP3 and FLAC formats.
 
-Filenames must be in the format: 'Artist - Title.mp3' or 'Artist - Title.flac'.
-(spaces, dashes, and multiple words are fine; but must contain " - " to delimit
-Artist and Title.  Existing metadata is deleted and new tags (Artist and Title only)
-are derived from the filename.
+Filenames must be in the format: "ARTIST - TITLE.mp3" or "ARTIST - TITLE.flac".
+(spaces, dashes, and multiple words are fine, but must contain " - " to delimit
+ARTIST and TITLE.  Existing metadata is deleted and new ARTIST/TITLE tags
+derived from the filename are saved.
+
+Requires:
+  * TagLib (audio metadata C++ library)
+  * pytaglib (python bindings for TagLib)
+
 """
 
 import argparse
 import logging
 import os
+import sys
 
 import taglib
 
@@ -24,34 +31,33 @@ logger = logging.getLogger(__name__)
 
 
 def parse_filename(filepath):
-    if not filepath.endswith(('.flac', '.mp3')):
-        logger.error('Invalid File Extension')
-        raise SystemExit
     filename = os.path.basename(filepath)
     basename = os.path.splitext(filename)[0]
     try:
         artist, title = basename.split(' - ', 1)
     except ValueError:
-        logger.error('No File Name Delimiter Found')
-        raise SystemExit
+        logger.error('No valid delimiter found in filename')
+        sys.exit(1)
     return artist, title
 
 
-def tag(filepath):
-    logger.info('Loading File: {}'.format(filepath))
+def tag(filepath, artist, title):
+    new_tags = {'ARTIST': [artist], 'TITLE': [title]}
     audio = taglib.File(filepath)
-    artist, title = parse_filename(filepath)
-    audio.tags = {'ARTIST': [artist], 'TITLE': [title]}
+    audio.tags = new_tags
+    audio.removeUnsupportedProperties(audio.unsupported)
     audio.save()
+    audio.close()
     logger.info('Tagged: {} - {}'.format(artist, title))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('dir', nargs='?', default=os.getcwd(),
-                        help='start directory')
+                        help='working directory')
     args = parser.parse_args()
     for filename in os.listdir(args.dir):
-        filepath = os.path.abspath(os.path.join(args.dir, filename))
-        tag(filepath)
-    logger.info('Done')
+        if filename.endswith(('.flac', '.mp3')):
+            filepath = os.path.abspath(os.path.join(args.dir, filename))
+            artist, title = parse_filename(filepath)
+            tag(filepath, artist, title)
