a94ccefe08b9a6103df615814e5e06ff7d7085d2
Corey Goldberg <cgoldberg@gmail.com>
2018-01-27 14:29:59 -0500

    updated instructions

--- tagging/audio_tag.py
+++ tagging/audio_tag.py
@@ -5,12 +5,14 @@
 """
 Clean and tag a directory of audio files.
 
-Supports MP3 and FLAC formats.
+This program processes a directory of MP3/FLAC audio files.
+It deletes existing metadata tags each track with an
+Artist and Title derived from the file's name.
 
-Filenames must be in the format: "ARTIST - TITLE.mp3" or "ARTIST - TITLE.flac".
-(spaces, dashes, and multiple words are fine, but must contain " - " to delimit
-ARTIST and TITLE.  Existing metadata is deleted and new ARTIST/TITLE tags
-derived from the filename are saved.
+Files must be named in the format:
+`ARTIST - TITLE.mp3` or `ARTIST - TITLE.flac`
+That is, they must contain " - " to delimit artist and title,
+and end with a valid extension (".mp3" or ".flac").
 
 Requires:
   * TagLib (audio metadata C++ library)
@@ -30,9 +32,8 @@ logging.basicConfig(level=logging.INFO, format='%(message)s')
 logger = logging.getLogger(__name__)
 
 
-def parse_filename(filepath):
-    filename = os.path.basename(filepath)
-    basename = os.path.splitext(filename)[0]
+def get_tags_from_filename(filepath):
+    basename = os.path.splitext(os.path.basename(filepath))[0]
     try:
         artist, title = basename.split(' - ', 1)
     except ValueError:
@@ -59,5 +60,5 @@ if __name__ == '__main__':
     for filename in os.listdir(args.dir):
         if filename.endswith(('.flac', '.mp3')):
             filepath = os.path.abspath(os.path.join(args.dir, filename))
-            artist, title = parse_filename(filepath)
+            artist, title = get_tags_from_filename(filepath)
             tag(filepath, artist, title)
